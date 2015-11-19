6111643b2d2dcbca1ba37f24a733d3a2c0d377eb
Samicelus <samicelus@hotmail.com>
2015-11-19 09:56:26 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,16 +1,21 @@
 var http = require('http');
 var url = require('url');
 var net = require('net');
+var fs = require('fs');
 
-function start(route, handler) {
-	http.createServer(function(request, response) {
-        var pathname = url.parse(request.url).pathname;
-        // 路由到相应的业务逻辑
-        route (pathname, handler, response, request);
-    }).listen(5901);
-	console.log('server is starting at 5901');
-
-	}
+fs.readFile('./config.json', function (err, data) {
+	if (err) throw err;
+	uploadServerPort = JSON.parse(data).uploadServerPort;
 	
+	function start(route, handler) {
+		http.createServer(function(request, response) {
+	        var pathname = url.parse(request.url).pathname;
+	        // 路由到相应的业务逻辑
+	        route (pathname, handler, response, request);
+	    }).listen(uploadServerPort);
+		console.log('server is starting at 5901');
+	
+		}
+	});	
 
 exports.start = start;
